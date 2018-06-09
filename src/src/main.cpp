60d1b5caf4dcb4d1f9a7bdf7ad3d308a0c255108
0x1001 <damian.nowok@gmail.com>
2018-06-10 00:40:06 +0200

    SD card demo works

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -26,22 +26,109 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include <SD.h>
 #include "epd4in2.h"
-//#include "imagedata.h"
 #include "epdpaint.h"
 
 #define COLORED     0
 #define UNCOLORED   1
 
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+File myFile;
+
+#define SD_CHIP_SELECT 6
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Epd epd;
 
-  delay(100);
-  Serial.print("e-Paper start");
+  delay(5);
+
+  Serial.println("e-Paper start");
+
+  pinMode(SS, OUTPUT);
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  while (!card.init(SPI_HALF_SPEED, SD_CHIP_SELECT)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* Is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+  }
+
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
+  if (!SD.begin(SD_CHIP_SELECT)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+
+  myFile = SD.open("TEST.TXT");
+
+  if (myFile) {
+  while (myFile.available()) {
+    Serial.write(myFile.read());
+  }
+  myFile.close();
+}
+// if the file isn't open, pop up an error:
+else {
+  Serial.println("error opening test.txt");
+}
+
+  Epd epd;
   if (epd.Init() != 0) {
-    Serial.print("e-Paper init failed");
+    Serial.println("e-Paper init failed");
     return;
   }
 
@@ -58,6 +145,25 @@ void setup() {
   unsigned char image[1400];
   Paint paint(image, 400, 28);    //width should be the multiple of 8
 
+  //epd.ClearFrame();
+  //epd.DisplayFrame();
+
+  paint.Clear(UNCOLORED);
+  for(int i=0; i < 400; i++)
+  {
+    for(int j=0; j < 28; j++)
+    {
+      paint.DrawAbsolutePixel(i, j, COLORED);
+    }
+  }
+
+  epd.SetPartialWindow(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+
+  //epd.SetPartialWindow(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+  //epd.SetPartialWindow(paint.GetImage(), 0, 28, paint.GetWidth(), paint.GetHeight());
+  //epd.SetPartialWindow(paint.GetImage(), 0, 2*28, paint.GetWidth(), paint.GetHeight());
+
+  /*
   paint.Clear(UNCOLORED);
   paint.DrawStringAt(0, 0, "e-Paper Demo", &Font24, COLORED);
   epd.SetPartialWindow(paint.GetImage(), 100, 40, paint.GetWidth(), paint.GetHeight());
@@ -86,6 +192,7 @@ void setup() {
   paint.Clear(UNCOLORED);
   paint.DrawFilledCircle(32, 32, 30, COLORED);
   epd.SetPartialWindow(paint.GetImage(), 200, 200, paint.GetWidth(), paint.GetHeight());
+  */
 
   /* This displays the data from the SRAM in e-Paper module */
   epd.DisplayFrame();
